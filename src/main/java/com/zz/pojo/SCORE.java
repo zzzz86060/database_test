ea00cc2d58a535458fecf901db003a95ad2bcc2c
百璃anki <137508190+zzzz86060@users.noreply.github.com>
2023-09-04 21:19:50 +0800

    pojo(get

---
+++ src/main/java/com/zz/pojo/SCORE.java
@@ -0,0 +1,10 @@
+package com.zz.pojo;
+
+import lombok.Data;
+
+@Data
+public class SCORE {
+    char sno;
+    char cno;
+    int grade;
+}
---
+++ src/main/java/com/zz/pojo/STUDENT.java
@@ -0,0 +1,14 @@
+package com.zz.pojo;
+
+import lombok.Data;
+
+@Data
+public class STUDENT {
+    char sno;
+    char sname;
+    char ssex;
+    char sbirthday;
+    char speciality;
+    char sclass;
+    int tc;
+}
