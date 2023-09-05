f8424e59f91bba11dd3cf2e7215793aa0d6d0f36
百璃anki <137508190+zzzz86060@users.noreply.github.com>
2023-09-05 22:02:26 +0800

    dao

---
+++ src/main/java/com/zz/EeApplication.java
@@ -0,0 +1,16 @@
+package com.zz;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@MapperScan("com.lantu.*.mapper")
+public class EeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EeApplication.class, args);
+    }
+
+
+}
