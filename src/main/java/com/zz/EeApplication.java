6e5d6ffbffb2f4a266b3741a6740f247f9467179
百璃anki <137508190+zzzz86060@users.noreply.github.com>
2023-09-06 11:51:37 +0800

    dao

--- src/main/java/com/zz/EeApplication.java
+++ src/main/java/com/zz/EeApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.lantu.*.mapper")
+@MapperScan("com.zz.mapper")
 public class EeApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/zz/mapper/studentMapper.java
+++ src/main/java/com/zz/mapper/studentMapper.java
@@ -1,4 +1,10 @@
 package com.zz.mapper;
 
+import com.zz.pojo.STUDENT;
+
+import java.util.List;
+
 public interface studentMapper {
+    int addStudent();
+     List<STUDENT> getAllStudent();
 }
